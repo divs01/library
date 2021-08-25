974d17f0967937d5b713dbe228ef5e2326921b3c
divs01 <86909768+divs01@users.noreply.github.com>
2021-08-25 13:15:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Online Library Management System
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style type="text/css">
+	nav
+	{
+		float: right;
+		word-spacing: 15px;
+		padding: 20px;
+	}
+	nav li 
+	{
+		display: inline-block;
+		line-height: 80px;
+	}
+</style>
+</head>
+
+
+<body>
+	<div class="wrapper">
+		<header>
+		<div class="logo">
+			<img src="images/9.png">
+			<h1 style="color: white;">ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
+		</div>
+
+		<?php
+		if(isset($_SESSION['login_user']))
+		{
+			?>
+				<nav>
+					<ul>
+						<li><a href="index.php">HOME</a></li>
+						<li><a href="books.php">BOOKS</a></li>
+						<li><a href="logout.php">LOGOUT</a></li>
+						<li><a href="feedback.php">FEEDBACK</a></li>
+					</ul>
+				</nav>
+			<?php
+		}
+		else
+		{
+			?>
+						<nav>
+							<ul>
+								<li><a href="index.php">HOME</a></li>
+								<li><a href="books.php">BOOKS</a></li>
+								<li><a href="student_login.php">LOGIN</a></li>
+								<li><a href="registration.php">SIGN-UP</a></li>
+								<li><a href="feedback.php">FEEDBACK</a></li>
+							</ul>
+						</nav>
+		<?php
+		}
+			
+		?>
+
+			
+		</header>
+		<section>
+		<div class="">
+			<br><br><br>
+			<div class="box">
+				<br><br><br><br>
+				<h1 style="text-align: center; font-size: 60px;">Welcome to Library</h1><br><br>
+				
+			</div>
+		</div>
+		</section>
+		
+
+	</div>
+	
+</body>
+</html>
